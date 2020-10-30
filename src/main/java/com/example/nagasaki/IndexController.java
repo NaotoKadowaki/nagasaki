ed29d10feef39b7948c0f8b3c29256a5d641db37
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-30 13:24:57 +0900

    10.30Lesson 川崎掲示板　録画有

--- src/main/java/com/example/nagasaki/IndexController.java
+++ src/main/java/com/example/nagasaki/IndexController.java
@@ -2,21 +2,42 @@ package com.example.nagasaki;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
-    @RequestMapping(value="/", method= RequestMethod.GET)
-    public String indexGet(Model model) {
-        model.addAttribute("name", "長崎通信です！");
-        return "index";
-    }
-    @RequestMapping(value="/nagasaki", method= RequestMethod.GET)
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public String indexGet(Model model) {
+//        model.addAttribute("name", "長崎通信です！");
+//        return "index";
+//    }
+
+    @RequestMapping(value = "/nagasaki", method = RequestMethod.GET)
     @ResponseBody
     public String nagasakiGet() {
 
         return "nagasaki";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexGet(Model model) {
+        model.addAttribute("form", new Form());
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String indexPost(@ModelAttribute Form form, Model model) {
+        System.out.println(form.getName());
+        System.out.println(form.getContent());
+        model.addAttribute("form", new Form());
+
+        model.addAttribute("name", form.getName());
+        model.addAttribute("content", form.getContent());
+
+        return "index2";
     }
 }
